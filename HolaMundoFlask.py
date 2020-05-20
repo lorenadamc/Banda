cf7dcd0d93b32dc19bd68a8144e8b0a4afba8ce8
Daniel <dfarenasm@correo.udistrital.edu.co>
2020-05-19 20:06:08 -0500

    Herencia Plantillas

--- HolaMundoFlask.py
+++ HolaMundoFlask.py
@@ -1,8 +1,9 @@
 from flask import Flask
+from flask import render_template
 app = Flask(__name__)
 @app.route('/')
-
 def index():
-    return 'Hola Mundo'
+    lista = ["Bajo","Guitarra","Flauta"]
+    return render_template('index.html', lista=lista)
 if __name__ == '__main__':
-    app.run(debug = True, port = 5000 )
+      app.run(debug = True, port = 5000 )
