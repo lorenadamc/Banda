4cc746151dd3c92aa05cbadea607b6998ad544c9
Daniel <dfarenasm@correo.udistrital.edu.co>
2020-05-20 20:47:44 -0500

    Cambios En Banda

--- AppBanda.py
+++ AppBanda.py
@@ -3,23 +3,23 @@ from flask import render_template, session, request
 from NuevaBanda import nuevabanda
 app = Flask(__name__)
 app.secret_key = 'esto-es-una-clave-muy-secreta'
-b = nuevabanda()
+banda = nuevabanda()
 @app.route('/')
 def index():
     session['BandaSesion'] = nuevabanda()
     b = session['BandaSesion']
-    return render_template('index.html', b = b)
+    return render_template('index.html', banda = b)
 @app.route('/banda')
 def Banda():
     session['BandaSesion'] = nuevabanda()
     b = session['BandaSesion']
-    return render_template('Instrumento.html', b = b)
+    return render_template('Instrumento.html', banda = b)
 @app.route('/preparar')
 def preparar():
     b = session['BandaSesion']
-    return render_template('preparar.html', b = b)
+    return render_template('preparar.html', banda = b)
 @app.route('/tocar')
 def tocar():
-    return render_template('tocando.html', b = b)
+    return render_template('tocando.html', banda = b)
 if __name__ == '__main__':
       app.run(debug = True, port = 5000 )
